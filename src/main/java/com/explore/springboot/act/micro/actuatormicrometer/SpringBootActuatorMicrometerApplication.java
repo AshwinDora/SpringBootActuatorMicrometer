5f00d652cea7e6c4dda2058ea51fae803eb65125
Dora <Doraa@labcorp.com>
2019-04-04 12:01:28 -0400

    initial raw commit

---
+++ src/main/java/com/explore/springboot/act/micro/actuatormicrometer/SpringBootActuatorMicrometerApplication.java
@@ -0,0 +1,13 @@
+package com.explore.springboot.act.micro.actuatormicrometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorMicrometerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActuatorMicrometerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/explore/springboot/act/micro/actuatormicrometer/SpringBootActuatorMicrometerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.explore.springboot.act.micro.actuatormicrometer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootActuatorMicrometerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
